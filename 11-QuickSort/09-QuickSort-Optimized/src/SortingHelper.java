ae57da0e2673f8fb2b92cb4174e88d476ce52adc
zozospider <2642220896@qq.com>
2020-12-07 21:03:37 +0800

    update play-algorithms-and-data-structures

---
+++ 11-QuickSort/09-QuickSort-Optimized/src/SortingHelper.java
@@ -0,0 +1,52 @@
+// 排序帮助类
+public class SortingHelper {
+
+    private SortingHelper() {
+    }
+
+    // 判断数组是否有序
+    public static <E extends Comparable<E>> boolean isSorted(E[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1].compareTo(arr[i]) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 测试排序耗时
+    public static <E extends Comparable<E>> long sort(E[] arr, String sortName) {
+        // 记录开始时间 (单位: 毫秒)
+        long start = System.currentTimeMillis();
+
+        if ("SelectionSort".equals(sortName)) {
+            // 选择排序
+            SelectionSort.sort(arr);
+        } else if ("SelectionSort2".equals(sortName)) {
+            // 选择排序
+            SelectionSort.sort2(arr);
+        } else if ("InsertionSort".equals(sortName)) {
+            // 插入排序
+            InsertionSort.sort(arr);
+        } else if ("InsertionSort2".equals(sortName)) {
+            // 插入排序
+            InsertionSort.sort2(arr);
+        } else if ("MergeSort".equals(sortName)) {
+            // 归并排序
+            MergeSort.sort(arr);
+        } else if ("QuickSort".equals(sortName)) {
+            // 快速排序
+            QuickSort.sort(arr);
+        }
+
+        // 记录结束时间 (单位: 毫秒)
+        long end = System.currentTimeMillis();
+
+        if (!isSorted(arr)) {
+            throw new RuntimeException(sortName + " failed");
+        }
+        // 返回时间间隔 (单位: 毫秒)
+        return end - start;
+    }
+
+}
