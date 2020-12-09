7fa56a9aecdc81e70caac87551ea9b9cb39420ec
zozospider <2642220896@qq.com>
2020-12-09 22:44:36 +0800

    update play-algorithms-and-data-structures

--- 02-Linear-Search/09-Test-Performance/src/LinearSearch.java
+++ 02-Linear-Search/09-Test-Performance/src/LinearSearch.java
@@ -5,7 +5,7 @@ public class LinearSearch {
     }
 
     // 线性查找
-    // 返回 data 数组中, target 所在的索引, 如果 target 不存在则返回 -1
+    // 在数组 data 中查找 target 所在的索引, 如果 target 不存在则返回 -1
     // 时间复杂度: O(n)
     public static <E> int search(E[] data, E target) {
         for (int i = 0; i < data.length; i++) {
