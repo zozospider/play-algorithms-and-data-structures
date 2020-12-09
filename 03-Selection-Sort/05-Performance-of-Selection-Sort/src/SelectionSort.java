9b7459de7877dbd65786ae6c63f004f190289cfa
zozospider <2642220896@qq.com>
2020-12-09 17:27:59 +0800

    update play-algorithms-and-data-structures

--- 03-Selection-Sort/05-Performance-of-Selection-Sort/src/SelectionSort.java
+++ 03-Selection-Sort/05-Performance-of-Selection-Sort/src/SelectionSort.java
@@ -10,16 +10,16 @@ public class SelectionSort {
         sort(arr, 0, arr.length - 1);
     }
 
-    // 选择排序: 对数组 arr 中的区间 arr[l, r] 进行排序
+    // 选择排序: 对数组 arr 中的区间 arr[left, right] 进行排序
     // 时间复杂度: O(1/2 * n^2 + 1/2 * n) = O(n^2)
-    public static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
+    public static <E extends Comparable<E>> void sort(E[] arr, int left, int right) {
 
-        // 循环不变量: arr[l, i) 已排序, arr[i, r] 未排序
-        for (int i = l; i <= r; i++) {
+        // 循环不变量: arr[left, i) 已排序, arr[i, right] 未排序
+        for (int i = left; i <= right; i++) {
 
-            // 循环体: arr[i, r] 未排序, 将 arr[i, r] 中的最小值要放到 arr[i] 的位置
+            // 循环体: arr[i, right] 未排序, 将 arr[i, right] 中的最小值要放到 arr[i] 的位置
             int minIndex = i;
-            for (int j = i; j <= r; j++) {
+            for (int j = i; j <= right; j++) {
                 if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
--- 04-Insertion-Sort/02-Insertion-Sort/src/SelectionSort.java
+++ 04-Insertion-Sort/02-Insertion-Sort/src/SelectionSort.java
@@ -10,16 +10,16 @@ public class SelectionSort {
         sort(arr, 0, arr.length - 1);
     }
 
-    // 选择排序: 对数组 arr 中的区间 arr[l, r] 进行排序
+    // 选择排序: 对数组 arr 中的区间 arr[left, right] 进行排序
     // 时间复杂度: O(1/2 * n^2 + 1/2 * n) = O(n^2)
-    public static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
+    public static <E extends Comparable<E>> void sort(E[] arr, int left, int right) {
 
-        // 循环不变量: arr[l, i) 已排序, arr[i, r] 未排序
-        for (int i = l; i <= r; i++) {
+        // 循环不变量: arr[left, i) 已排序, arr[i, right] 未排序
+        for (int i = left; i <= right; i++) {
 
-            // 循环体: arr[i, r] 未排序, 将 arr[i, r] 中的最小值要放到 arr[i] 的位置
+            // 循环体: arr[i, right] 未排序, 将 arr[i, right] 中的最小值要放到 arr[i] 的位置
             int minIndex = i;
-            for (int j = i; j <= r; j++) {
+            for (int j = i; j <= right; j++) {
                 if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
@@ -28,21 +28,21 @@ public class SelectionSort {
         }
     }
 
-    // 选择排序 (另一种方式: 从后往前): 对数组 arr 中的区间 arr[l, r] 进行排序
+    // 选择排序 (另一种方式: 从后往前): 对数组 arr 中的区间 arr[left, right] 进行排序
     // 时间复杂度: O(n^2)
     public static <E extends Comparable<E>> void sort2(E[] arr) {
 
         sort2(arr, 0, arr.length - 1);
     }
 
-    public static <E extends Comparable<E>> void sort2(E[] arr, int l, int r) {
+    public static <E extends Comparable<E>> void sort2(E[] arr, int left, int right) {
 
-        // 循环不变量: arr(i, r] 已排序, arr[l, i] 未排序
-        for (int i = r; i >= l; i--) {
+        // 循环不变量: arr(i, right] 已排序, arr[left, i] 未排序
+        for (int i = right; i >= left; i--) {
 
-            // 循环体: arr[l, i] 未排序, 将 arr[l, i] 中的最大值要放到 arr[i] 的位置
+            // 循环体: arr[left, i] 未排序, 将 arr[left, i] 中的最大值要放到 arr[i] 的位置
             int maxIndex = i;
-            for (int j = i; j >= l; j--) {
+            for (int j = i; j >= left; j--) {
                 if (arr[j].compareTo(arr[maxIndex]) > 0) {
                     maxIndex = j;
                 }
